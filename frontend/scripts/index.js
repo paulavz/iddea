1a1119e90f3d31b11444e3aef461fedd26873d3d
unknown <paula.sua.008@gmail.com>
2021-08-22 23:10:19 -0400

    En proceso de la sección proyectos(mobile)

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -7,4 +7,7 @@ import arrowdown from '../icons/arrow-down.svg';
 import infinito from '../img/infinito.svg';
 import adorno from '../icons/adorno.svg';
 import ilustracion from '../img/ilustracion.svg';
-console.log("Hola Mundo");
+import tree from '../icons/tree.svg';
+import reloj from '../icons/reloj.svg';
+import money from '../icons/money.svg';
+import imanes from '../img/imanes.png';
