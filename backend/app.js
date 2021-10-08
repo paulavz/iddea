2e69f9ebfdf27b9b891d8ff621c2e3b8a6f00103
unknown <paula.sua.008@gmail.com>
2021-10-08 08:15:56 -0400

    node add

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});*/
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("puerto", process.env.PORT || 4200);
+app.listen(app.get("puerto"), () => {
+  console.log("Example app listening on port " + app.get("puerto"));
+});
