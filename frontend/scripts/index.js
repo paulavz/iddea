eaf0ba83eb703b0e4e727312d4168ff8efbf0c51
unknown <paula.sua.008@gmail.com>
2021-10-08 10:46:15 -0400

    CSS Fixes

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,21 +1,20 @@
 /****************
     Images
 ****************/
-import '../styles/styles.css';
-import '../styles/languajes.css';
-import menu from '../icons/menu.svg';
-import arrowdown from '../icons/arrow-down.svg';
-import infinito from '../img/infinito.svg';
-import adorno from '../icons/adorno.svg';
-import ilustracion from '../img/ilustracion.svg';
-import tree from '../icons/tree.svg';
-import reloj from '../icons/reloj.svg';
-import money from '../icons/money.svg';
-import imanes from '../img/imanes.png';
-import instagram from '../icons/instagram.svg';
-import linkedin from '../icons/linkedin.svg';
-import facebook from '../icons/facebook.svg';
-import binance from '../icons/binance.svg';
-import paypal from '../icons/paypal.svg';
-
-
+import "../styles/styles.css";
+import "../styles/languajes.css";
+import "../styles/infinity.css";
+import menu from "../icons/menu.svg";
+import arrowdown from "../icons/arrow-down.svg";
+import infinito from "../img/infinito.svg";
+import adorno from "../icons/adorno.svg";
+import ilustracion from "../img/ilustracion.svg";
+import tree from "../icons/tree.svg";
+import reloj from "../icons/reloj.svg";
+import money from "../icons/money.svg";
+import instagram from "../icons/instagram.svg";
+import linkedin from "../icons/linkedin.svg";
+import facebook from "../icons/facebook.svg";
+import binance from "../icons/binance.svg";
+import paypal from "../icons/paypal.svg";
+import imanes from "../img/imanes.png";
