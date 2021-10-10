d76a1f28b1789d66ed01551adf81f8a8a52625b1
unknown <paula.sua.008@gmail.com>
2021-10-10 14:19:25 -0400

    Commit with frontend

---
+++ frontend/scripts/lazyload.js
@@ -0,0 +1,18 @@
+const initImg = document.getElementById("home-background");
+const fondo1 = document.getElementById("about");
+const fondo2 = document.getElementById("contact");
+
+window.addEventListener("load", function (event) {
+	fetch("https://iddea.herokuapp.com/images/rect853.png")
+		.then((resp) => resp.blob())
+		.then((blob) => {
+			const url = URL.createObjectURL(blob);
+			initImg.style.background = `#f3f3f3 url(${url}) no-repeat`;
+			initImg.style.backgroundPosition = "center";
+			initImg.style.backgroundSize = "cover";
+		})
+		.then(() => {
+			fondo1.classList.add("aboutus-img");
+			fondo2.classList.add("contact-img");
+		});
+});
