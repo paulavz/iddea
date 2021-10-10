d4ae9191266f4210fd71a1e9fc1aaafd9abb5016
unknown <paula.sua.008@gmail.com>
2021-10-10 17:36:15 -0400

    Complete lazy load

--- frontend/scripts/lazyload.js
+++ frontend/scripts/lazyload.js
@@ -1,19 +1,35 @@
 const initImg = document.getElementById("home-background");
-const fondo1 = document.getElementById("about");
-const fondo2 = document.getElementById("contact");
+const fondo1 = document.getElementById("about-background");
+const fondo2 = document.getElementById("contact-background");
 
-window.addEventListener("load", function (event) {
-	fetch(
-		"https://res.cloudinary.com/mismangas/image/upload/v1633895412/rect853_xiondx.png"
-	)
+const lazyLoad = (container, urlImage, classImage) => {
+	return fetch(urlImage)
 		.then((resp) => resp.blob())
 		.then((blob) => {
 			const url = URL.createObjectURL(blob);
-			initImg.style.background = `#f3f3f3 url(${url}) no-repeat`;
-			initImg.style.backgroundPosition = "center";
-			initImg.style.backgroundSize = "cover";
-			initImg.classList.add("principal-height");
+			container.style.background = `#f3f3f3 url(${url}) no-repeat`;
+			container.style.backgroundPosition = "center";
+			container.style.backgroundSize = "cover";
+			container.classList.add(classImage);
 		});
-	fondo1.classList.add("aboutus-img");
-	fondo2.classList.add("contact-img");
+};
+
+window.addEventListener("load", function (event) {
+	lazyLoad(
+		initImg,
+		"https://iddea.herokuapp.com/images/rect853.png",
+		"principal-height"
+	).then(() => {
+		lazyLoad(
+			fondo1,
+			"https://iddea.herokuapp.com/images/fondo2.png",
+			"about-height"
+		).then(() => {
+			lazyLoad(
+				fondo2,
+				"https://iddea.herokuapp.com/images/fondo3.png",
+				"contact-height"
+			);
+		});
+	});
 });
