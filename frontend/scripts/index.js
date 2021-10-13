c32aa88f0421bf6069eb3b54add92a0c36ad1fa6
unknown <paula.sua.008@gmail.com>
2021-10-12 23:48:43 -0400

    Menu left

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -5,6 +5,7 @@ import "../styles/styles.css";
 import "../styles/languajes.css";
 import "../styles/infinity.css";
 import "./lazyload.js";
+import "./menu.js";
 import "./formContact";
 import menu from "../icons/menu.svg";
 import arrowdown from "../icons/arrow-down.svg";
